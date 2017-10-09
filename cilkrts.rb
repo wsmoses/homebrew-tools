e99067864e6d65c3c1976cb13a55da8283d2e3a4
William S. Moses <gh@wsmoses.com>
2017-10-08 21:46:31 -0400

    Cmake flags

--- cilkrts.rb
+++ cilkrts.rb
@@ -30,8 +30,8 @@ class Cilkrts < Formula
     args = %W[
       -DCMAKE_INSTALL_PREFIX=#{install_prefix}
       -DCMAKE_C_COMPILER=clang-5.0
-      -DCMAKE_CXX_COMPILER="clang++-5.0\ -stdlib=libc++"
-      -DCMAKE_CXX_FLAGS="-nostdinc++\ -I/usr/local/opt/tapir/lib/llvm-5.0/include/c++/v1"
+      -DCMAKE_CXX_COMPILER=clang++-5.0
+      -DCMAKE_CXX_FLAGS="-stdlib=libc++\ -nostdinc++\ -I/usr/local/opt/tapir/lib/llvm-5.0/include/c++/v1"
       -DCMAKE_LD_FLAGS="-L/usr/local/opt/tapir/lib/llvm-5.0/lib"
     ]
 
